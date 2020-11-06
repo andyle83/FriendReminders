3e81a3f447de2551f80729a8fab27010fcde9349
WGTECH\anh.le <anh.le@beteasy.com.au>
2020-11-06 23:51:28 +1100

    feat: add data object in return result

---
+++ Services/WebApi/Features/Reminders/DataContracts/ReminderResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WebApi.Features.Reminders.DataContracts
+{
+    public class ReminderResponse
+    {
+        public Guid Id { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime? RemiderTime { get; set; }
+    }
+}
